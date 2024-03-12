43fe9ce33dd10722a5029a1b4183612a600e9eb7
Brooklyn Christopher <brooklyn.christopher@ucalgary.ca>
2024-03-12 08:48:53 +0000

    Upload New File

---
+++ Server/MongoDB/schema/recipe.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+
+    _id: {type: String},
+    date: { type: Date, required: true},
+    name: { type: String, required: true},
+    description: {type: String},
+    trainerUsername: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}, //check trainer role
+    ingredients: [{type: mongoose.Schema.Types.ObjectId, ref: 'food'}]
+
+});
+
+module.exports = mongoose.model('recipe', recipeSchema);
