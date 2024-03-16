c700befec98da46cd75c951c3e0368b45dbbcc00
Ethan Card <michael.card@ucalgary.ca>
2024-03-16 21:24:54 +0000

    Update weight.js

--- Server/MongoDB/schema/weight.js
+++ Server/MongoDB/schema/weight.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const weightSchema = new mongoose.Schema({
-    _id: {type: String},
     date: { type: Date, required: true },
     measurement: {
         type: String, 
