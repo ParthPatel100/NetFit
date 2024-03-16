cf1b87e6c9067ec19a539f5d001895aaebe3b1b9
Ethan Card <michael.card@ucalgary.ca>
2024-03-16 21:22:52 +0000

    Update gym.js

--- Server/MongoDB/schema/gym.js
+++ Server/MongoDB/schema/gym.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 //for this schema not sure if we are going to include, but if we do we need a function to check the user has a trainer role
 const gymSchema = new mongoose.Schema({
-    _id: {type: String},
     date: { type: Date, required: true },
     name: { type: String, required: true},
     reps: { type: Number, min: 0},
