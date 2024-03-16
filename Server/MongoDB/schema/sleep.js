86cf3f2c7c52728f8330a21f2b0c6f221e162709
Ethan Card <michael.card@ucalgary.ca>
2024-03-16 21:23:54 +0000

    Update sleep.js

--- Server/MongoDB/schema/sleep.js
+++ Server/MongoDB/schema/sleep.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const sleepSchema = new mongoose.Schema({
-    _id: {type: String},
     date: { type: Date, required: true },
     startTime: { type: String },
     duration: { type: Number, required: true}
