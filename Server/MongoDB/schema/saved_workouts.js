e19f102eecaec05483a5231f424182ffce3c7cd9
Ethan Card <michael.card@ucalgary.ca>
2024-03-21 04:09:05 +0000

    Update saved_workouts.js

--- Server/MongoDB/schema/saved_workouts.js
+++ Server/MongoDB/schema/saved_workouts.js
@@ -1,5 +1,5 @@
 const savedWorkoutSchema = new mongoose.Schema({ 
     post_id: [{type: mongoose.Schema.Types.ObjectId, ref: 'post'}],
-    user: [{type: mongoose.Schema.Types.ObjectId, ref: 'user'}],
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 });
 module.exports = mongoose.model('SavedWorkout', savedWorkoutSchema);
