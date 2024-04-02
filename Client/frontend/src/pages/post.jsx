3368cfd9162a710a609f2b3a176ce2a4b6d9815b
OsamaBamatraf <owaheib@gmail.com>
2024-04-02 17:11:49 -0600

    updated post page to be mobile responsive

--- Client/frontend/src/pages/post.jsx
+++ Client/frontend/src/pages/post.jsx
@@ -77,102 +77,93 @@ export default function PostPage(){
 
     */}
 
-    return(
-
-        <div className="bg-gray-100 h-screen">
-            {/* */}
-            {/*Card */}
-            <div className='grid'>
-                <div className="flex-1 grid grid-rows-12 text-center ml-60 mr-10 mt-20 shadow-xl">
-                    <div className="bg-white p-2 rounded-t-2xl text-xl text-left pl-4 border-b border-gray-300 border-opacity-70">New Post</div>
-                    <div className="bg-white p-2 row-span-10">
-                            {/*inner grid of card */}
-                            <div className='grid'>
-                                <div className="flex-1 grid grid-rows-10 grid-child gap-3 text-center ">
-                                    <div className="border-2 border-gray-300 border-opacity-70 rounded-xl row-span-5 ml-3 mr-3 mt-2 p-2.5 text-left">
-                                        {/* Title and caption inside grid cell*/}
-                                        <div className={"flex flex-col"}>
-                                            <p className="ml-1 text-left text-sm">Title</p>
-                                            <input type="title"
-                                                rows = "2"
-                                                className={"block w-full border-[1px] bg-neutral-100 border-gray-300 rounded-[5px] p-2 mt-1 mb-3 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 text-xs"}
-                                                placeholder={"Enter Post Title"}
-                                                value={title}
-                                                onChange={(e) => setTitle(e.target.value)}>
-                                            </input>
-                                            <p className="ml-1.5 text-left text-sm">Caption</p>
-                                            <textarea
-                                                className={"w-full resize-y border-[1px] bg-neutral-100 border-gray-300 rounded-[5px] p-2 mt-1 focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500 text-xs"}
-                                                style={{ boxSizing: "border-box" }}
-                                                placeholder={"Enter Caption Title"}
-                                                value={caption}
-                                                onChange={(e) => setCaption(e.target.value)}>
-                                            </textarea>
-                                        </div>
-                        
-                                    </div>
-                                    {/* add images card */}
-                                    <div className="border-2 border-gray-300 border-opacity-70 p-2 rounded-xl row-span-4 ml-3 mr-3 text-left pl-4">
-                                        <p className="ml-1.5 text-left text-sm">Add Images</p>
-                                        <div className="flex h-5/6">
-                                        {images.map((imageUrl, index) => (
-                                            <div
-                                            key={index}
-                                            className="relative w-1/3 m-1.5 border border-gray-300 rounded-xl text-center"
-                                            style={{ 
-                                                backgroundImage: `url(${imageUrl})`,
-                                                backgroundSize: "cover",
-                                                backgroundPosition: "center",
-                                            }}
-                                            >
-                                            <button
-                                                className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300"
-                                                onClick={() => handleRemoveImage(index)}
-                                            >
-                                                <Trash2 
-                                                
-                                                    style={{color: "#fff" }}
-                                                
-                                                />
-                                            </button>
-                                            </div>
-                                        ))}
-                                        {images.length < 6 && (
-                                            <button
-                                            className="w-1/3 m-1.5 border border-gray-300 rounded-xl text-center"
-                                            onClick={() => document.getElementById("imageUpload").click()}
-                                            >
-                                            +
-                                            </button>
-                                        )}
-                                        <input
-                                            id="imageUpload"
-                                            type="file"
-                                            accept="image/*"
-                                            className="hidden"
-                                            onChange={handleInputChange}
-                                        />
-                                        </div>
-                                    </div>
-                                    
-                                    {/* fitness card*/}
-                                    <div className="border-2 border-gray-300 border-opacity-70 p-2 rounded-xl row-span-1 ml-3 mr-3 text-left pl-4">Fitness Data</div>
+    return (
+        <div className="bg-gray-100 h-screen md:mt-14 mt-0 md:ml-[12rem]">
+            <div className='flex flex-col items-center pt-6'>
+                <div className="flex flex-col w-3/4 shadow-xl">
+                    <div className="bg-white p-2 rounded-t-2xl text-xl text-left pl-4 border-b border-gray-300">New Post</div>
+                   
+                    {/*inner grid of card */}
+                    <div className="bg-white p-2 flex-1">
+                        <div className='flex flex-col gap-3 p-3'>
+                            <div className="border-2 border-gray-300 rounded-xl p-2.5 text-left">
+                                
+                            {/* Title and caption inside grid cell*/}
+                                <div className="flex flex-col">
+                                    <label className="ml-1 text-sm">Title</label>
+                                    <input type="text"
+                                        className="block w-full bg-neutral-100 border-gray-300 rounded-[5px] p-2 mt-1 mb-3 focus:outline-none focus:border-purple-500"
+                                        placeholder="Enter Post Title"
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                    />
+                                    <label className="ml-1 text-sm">Caption</label>
+                                    <textarea
+                                        className="w-full resize-y bg-neutral-100 border-gray-300 rounded-[5px] p-2 mt-1 focus:outline-none focus:border-purple-500"
+                                        placeholder="Enter Caption"
+                                        value={caption}
+                                        onChange={(e) => setCaption(e.target.value)}
+                                    />
                                 </div>
                             </div>
-                        </div>
-                    
-                    <div className="bg-white p-2 rounded-b-2xl ">
-                        <button
-                            className="bg-gradient-to-br from-purple-500 to-pink-500 w-full text-white border-gray-500 rounded-[10px] p-2 mr-15 ml-15 mt-1"
-                        >
+                            
 
+
+                            {/* add images card */}
+
+
+                            <div className="border-2 border-gray-300 rounded-xl p-2 text-left">
+                                <label className="ml-1 text-sm">Add Images</label>
+                                <div className="flex flex-wrap justify-start items-center h-auto">
+                                    {images.map((imageUrl, index) => (
+                                        <div
+                                            key={index}
+                                            className="relative m-1.5 border rounded-xl text-center bg-cover bg-center"
+                                            style={{
+                                                backgroundImage: `url(${imageUrl})`,
+                                                width: 'calc(33% - 12px)',
+                                                paddingBottom: 'calc(33% - 12px)',
+                                            }}
+                                        >
+                                            <button
+                                                className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300"
+                                                onClick={() => handleRemoveImage(index)}
+                                                style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+                                            >
+                                                <Trash2 className="text-white" />
+                                            </button>
+                                        </div>
+                                    ))}
+                                    {images.length < 4 && (
+                                        <button
+                                            className="m-1.5 border border-gray-300 rounded-xl text-center flex justify-center items-center"
+                                            onClick={() => document.getElementById("imageUpload").click()}
+                                            style={{ width: 'calc(33% - 12px)', height: 'calc(33% - 12px)' , minWidth: '100px', minHeight: '100px' }}
+                                        >
+                                            +
+                                        </button>
+                                    )}
+                                    <input
+                                        id="imageUpload"
+                                        type="file"
+                                        accept="image/*"
+                                        className="hidden"
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-gray-300 rounded-xl p-2 text-left">Fitness Data</div>
+                        </div>
+                    </div>
+                    <div className="bg-white p-2 rounded-b-2xl ">
+                        <button className="bg-gradient-to-br from-purple-500 to-pink-500 w-full text-white rounded-[10px] p-2">
+                            Submit
                         </button>
-                        
                     </div>
                 </div>
             </div>
         </div>
-        
     )
 }
 
